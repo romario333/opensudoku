64a56618be4647d0d472ccb6429d878f55ad94f9
Roman Masek <romario333@gmail.com>
2009-04-02 06:41:35 +0000

    Oops.

--- OpenSudoku/src/cz/romario/opensudoku/gui/FolderListActivity.java
+++ OpenSudoku/src/cz/romario/opensudoku/gui/FolderListActivity.java
@@ -48,6 +48,8 @@ public class FolderListActivity extends ListActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
+		setTitle("Puzzle folders");
+		
 		setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
 		
         // Inform the list we provide context menus for items
