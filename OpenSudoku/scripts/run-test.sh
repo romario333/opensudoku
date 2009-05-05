dfb104c370cc5a5d47ba08b6cd64d617db7e1b09
Roman Masek <romario333@gmail.com>
2009-05-05 15:18:26 +0000

    Unit test support.

---
+++ OpenSudoku/scripts/run-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+adb shell am instrument -w cz.romario.opensudoku/android.test.InstrumentationTestRunner
+
+exit 0
+
---
+++ OpenSudoku/tests/src/cz/romario/opensudoku/game/SudokuCellTest.java
@@ -0,0 +1,11 @@
+package cz.romario.opensudoku.game;
+
+import junit.framework.TestCase;
+
+public class SudokuCellTest extends TestCase {
+
+	public void testSudokuCell() {
+		fail("Not yet implemented");
+	}
+
+}
