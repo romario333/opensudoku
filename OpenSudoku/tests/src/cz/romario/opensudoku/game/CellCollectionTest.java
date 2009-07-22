a546986de81884d839477c5da0de00501e95a456
Roman Masek <romario333@gmail.com>
2009-07-22 10:44:07 +0000

    performance tests - some experiments

---
+++ OpenSudoku/tests/src/cz/romario/opensudoku/game/CellCollectionTest.java
@@ -0,0 +1,44 @@
+package cz.romario.opensudoku.game;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.telephony.CellLocation;
+import junit.framework.TestCase;
+
+public class CellCollectionTest extends TestCase {
+	
+	public void testCreateEmpty() {
+		CellCollection x = CellCollection.createEmpty();
+		
+		for (int r=0; r<CellCollection.SUDOKU_SIZE; r++) {
+			for (int c=0; c<CellCollection.SUDOKU_SIZE; c++) {
+				Cell cell = x.getCell(r, c);
+				assertEquals(0, cell.getValue());
+				assertEquals(false, cell.isEditable());
+				assertEquals(true, cell.isValid());
+			}
+		}
+	}
+	
+	public void testParcelableVsToString() {
+		CellCollection cells = CellCollection.createDebugGame();
+		cells.getCell(2, 2).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(1, 4).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(5, 4).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(7, 7).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(6, 6).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(6, 7).setNote(CellNote.fromString("5,7,"));
+		cells.getCell(7, 6).setNote(CellNote.fromString("5,7,"));
+		
+		Bundle bundle = new Bundle();
+		bundle.putParcelable("test", cells);
+		
+		
+		
+	}
+	
+
+	
+	
+
+}
