00fe8110df67794a384786e723441ccf27ce3ec0
Roman Masek <romario333@gmail.com>
2009-09-27 07:59:27 +0000

    Unit tests separated to their own project.

--- OpenSudoku/scripts/run-tests.sh
+++ OpenSudoku/scripts/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-adb shell am instrument -w cz.romario.opensudoku/android.test.InstrumentationTestRunner
+adb shell am instrument -w cz.romario.opensudoku.test/android.test.InstrumentationTestRunner
 
 # run single test:
 # adb shell am instrument -w -e class cz.romario.opensudoku.game.CellNoteTest cz.romario.opensudoku/android.test.InstrumentationTestRunner
